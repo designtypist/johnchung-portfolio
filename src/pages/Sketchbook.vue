7543f4bcf21da4f2827e06d373ff9a41bcb8fdfd
designtypist <designtypist@gmail.com>
2020-02-13 01:36:47 -0500

    Fixed the menu routing and added temporary content for Portfolio and Sketchbook pages.

--- src/pages/Sketchbook.vue
+++ src/pages/Sketchbook.vue
@@ -1,5 +1,12 @@
 <template>
   <Layout>
-      <!-- photo gallery -->
+      
+      <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
+        <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
+          <div class="leading-tight">Sketchbook</div>
+        </div>
+        <!-- photo gallery -->
+      </div>
+
   </Layout>
 </template>
