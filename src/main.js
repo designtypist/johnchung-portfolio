acca06e472d5f0d2180f3662864329b805f7985d
jchung <designtypist@gmail.com>
2020-04-14 16:08:13 -0400

    added the vue component Vue-Waterfall

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@
 import DefaultLayout from '~/layouts/Default.vue'
 import VueScrollTo from 'vue-scrollto'
 import VueFuse from 'vue-fuse'
+import Waterfall from 'vue-waterfall/lib/waterfall'
+import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.component({
+    'Waterfall': Waterfall,
+    'Waterfall-slot': WaterfallSlot
+  })
 
   Vue.use(VueScrollTo, {
     duration: 500,
@@ -36,5 +42,3 @@ export default function (Vue, { router, head, isClient }) {
     href: 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700'
   })
 }
-
-
