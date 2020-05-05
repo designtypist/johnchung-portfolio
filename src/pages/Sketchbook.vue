a711123486656c2356181c989690b7af60a510ee
jchung <designtypist@gmail.com>
2020-05-05 11:22:33 -0400

    added the Sketchbook image gallery functionality

--- src/pages/Sketchbook.vue
+++ src/pages/Sketchbook.vue
@@ -1,20 +1,86 @@
 <template>
   <Layout>
-      
-      <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
-        <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-          <div class="leading-tight">Sketchbook (temp)</div>
+
+      <div class="hero container mx-auto">
+        <div class="text-4xl font-bold text-center">
+          <div class="leading-tight">Sketchbook</div>
         </div>
-        <!-- photo gallery -->
+        <div class="gallery">
+          <div class="gallery-panel" v-for="edge in $page.sketchbookImages.edges" :key="edge.node.id">
+            <g-image :src="edge.node.image" :alt="edge.node.title" @click="show" />
+            <vue-easy-lightbox
+              moveDisabled
+              :visible="visible"
+              :imgs="edge.node.image.src"
+              @hide="handleHide"
+            ></vue-easy-lightbox>
+          </div>
+        </div>
       </div>
 
   </Layout>
 </template>
 
+<page-query>
+query SketchbookImages {
+  sketchbookImages: allGalleryImages (
+    filter: { gallery_type: { eq: "sketchbook" } } 
+    sortBy: "position", order: ASC
+  ) {
+    edges {
+      node  {
+        id
+        title
+        description
+        tags
+        position
+        filename
+        image
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import VueEasyLightbox from 'vue-easy-lightbox'
+
 export default {
   metaInfo: {
     title: 'Sketchbook'
+  },
+  components: {
+    VueEasyLightbox
+  },
+  data() {
+    return {
+      visible: false,
+    }
+  },
+  methods: {
+    show() {
+      this.visible = true
+    },
+    handleHide() {
+      this.visible = false
+    }
   }
 }
 </script>
+
+<style scoped>
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    grid-gap: 1rem;
+    max-width: 80rem;
+    margin: 2rem auto;
+    padding: 0 5rem;
+  }
+
+  .gallery-panel img {
+    width: 100%;
+    object-fit: inside;
+    cursor: pointer;
+  }
+</style>
