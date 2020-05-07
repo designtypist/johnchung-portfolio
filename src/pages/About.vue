9509444b3a7ca585bf8ec3b4b09ccda0811e2dcb
jchung <designtypist@gmail.com>
2020-05-07 14:56:46 -0400

    modified as well as styled the about page and did minor style changes to contact page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,16 @@
 <template>
   <Layout>
       
-    <div class="container mx-auto flex">
-      <div class="w-1/2 px-2">
-        <g-image src="../images/avatar.jpg" alt="avatar" class="rounded-full"/>
-      </div>
-      <div class="w-1/2 px-2">
-          <div class="my-6">
-            <h2 class="text-xl font-bold uppercase my-2">Summary</h2>
+    <div class="container">
+      <div class="container-inner mx-auto flex mt-8">
+        <div class="w-1/3 px-2 mr-6">
+          <div class="rounded">
+            <g-image class="w-full" src="../images/avatar.jpg" alt="Avatar" />
+          </div>
+        </div>
+        <div class="w-2/3 px-2">
+          <div class="mb-6">
+            <h2 class="text-xl font-bold uppercase mb-2">Summary</h2>
             <p>Hi, my name is John Chung. I'am an animator based in Toronto, Ontario. I love bringing things to life through animation and I can't imagine myself not 
               doing anything else. Feel free to contact me at <a href="mailto:johnchungart@gmail.com">johnchungart@gmail.com</a></p>
             <div class="my-6">
@@ -16,12 +19,32 @@
           </div>
           <div class="my-8">
             <h2 class="text-xl font-bold uppercase my-2">Software Proficiency</h2>
-            <software-proficency />
+            <div class="flex flex-wrap">
+              <div class="w-1/4" v-for="edge in $page.softwareProficiency.edges" :key="edge.node.id">
+                <SoftwareProficency :name="edge.node.name" :src="edge.node.image.src" />
+              </div>
+            </div>
           </div>
+        </div>
       </div>
     </div>
   </Layout>
 </template>
+
+<page-query>
+query softwareProficiency {
+  softwareProficiency : allSoftwareProficiency {
+     edges {
+      node  {
+        id
+        name
+        filename
+        image (quality: 80)
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 import SoftwareProficency from '../components/SoftwareProficency'
@@ -35,3 +58,9 @@ export default {
   }
 }
 </script>
+
+<style scroped>
+  .container-inner {
+    max-width: 1000px;
+  }
+</style>
