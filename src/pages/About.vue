7885ff3fa1ee40eb2fcc2bed7ccd4484f2a5e674
jchung <designtypist@gmail.com>
2020-05-10 22:50:12 -0400

    styled the majority for responsive mobile design

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,12 @@
 <template>
   <Layout>
       
-    <div class="container">
-      <div class="container-inner mx-auto flex mt-8">
-        <div class="w-1/3 px-2 mr-6">
-          <div class="rounded">
-            <g-image class="w-full" src="../images/avatar.jpg" alt="Avatar" />
-          </div>
+    <div class="container-inner mx-auto">
+      <div class="flex flex-wrap md:mx-4 mx-2">
+        <div class="md:w-1/3 w-full md:px-6 px-0 md:py-0 py-2">
+          <g-image class="md:float-right" src="../images/avatar.jpg" alt="Avatar" />
         </div>
-        <div class="w-2/3 px-2">
+        <div class="md:w-2/3 w-full">
           <div class="mb-6">
             <h2 class="text-xl font-bold uppercase mb-2">Summary</h2>
             <p>Hi, my name is John Chung. I'am an animator based in Toronto, Ontario. I love bringing things to life through animation and I can't imagine myself not 
@@ -20,7 +18,7 @@
           <div class="my-8">
             <h2 class="text-xl font-bold uppercase my-2">Software Proficiency</h2>
             <div class="flex flex-wrap">
-              <div class="w-1/4" v-for="edge in $page.softwareProficiency.edges" :key="edge.node.id">
+              <div class="md:w-1/4 w-1/3" v-for="edge in $page.softwareProficiency.edges" :key="edge.node.id">
                 <SoftwareProficency :name="edge.node.name" :src="edge.node.image.src" />
               </div>
             </div>
@@ -58,9 +56,3 @@ export default {
   }
 }
 </script>
-
-<style scroped>
-  .container-inner {
-    max-width: 1000px;
-  }
-</style>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,31 +1,29 @@
 <template>
   <Layout>
 
-      <div class="hero container mx-auto">
-        <div class="text-4xl font-bold text-center">
-          <div class="leading-tight">Portfolio</div>
+    <div>
+      <h1 class="text-4xl font-bold text-center">Portfolio</h1>
+      <div class="gallery">
+        <div class="gallery-panel" v-for="(edge, index) in $page.portfolioImages.edges" :key="edge.node.id">
+          <g-image :src="edge.node.image.src" :alt="edge.node.title"
+            @click="showMultiple(index)"
+            @load="onUpdateImgGallery(edge.node.image.src)" />
         </div>
-        <div class="gallery">
-          <div class="gallery-panel" v-for="(edge, index) in $page.portfolioImages.edges" :key="edge.node.id">
-            <g-image :src="edge.node.image.src" :alt="edge.node.title"
-             @click="showMultiple(index)"
-             @load="onUpdateImgGallery(edge.node.image.src)" />
-          </div>
-          <vue-easy-lightbox
-            moveDisabled
-            :visible="visible"
-            :imgs="imgs"
-            :index="index"
-            @hide="handleHide"
-          ></vue-easy-lightbox>
-        </div>
+        <vue-easy-lightbox
+          moveDisabled
+          :visible="visible"
+          :imgs="imgs"
+          :index="index"
+          @hide="handleHide"
+        ></vue-easy-lightbox>
       </div>
+    </div>
 
   </Layout>
 </template>
 
 <page-query>
-query PortfolioImages {
+query {
   portfolioImages: allGalleryImages (
     filter: { gallery_type: { eq: "portfolio" } } 
     sortBy: "position", order: ASC
@@ -46,51 +44,33 @@ query PortfolioImages {
 </page-query>
 
 <script>
-
-  export default {
-    metaInfo: {
-      title: 'Portfolio'
+export default {
+  metaInfo: {
+    title: 'Portfolio'
+  },
+  data() {
+    return {
+      imgs: '',
+      image_gallery: [],
+      index: 0,
+      visible: false,
+    }
+  },
+  methods: {
+    showMultiple(index) {
+      this.imgs = this.image_gallery
+      this.index = index 
+      this.show()
     },
-    data() {
-      return {
-        imgs: '',
-        image_gallery: [],
-        index: 0,
-        visible: false,
-      }
+    show() {
+      this.visible = true
     },
-    methods: {
-      showMultiple(index) {
-        this.imgs = this.image_gallery
-        this.index = index 
-        this.show()
-      },
-      show() {
-        this.visible = true
-      },
-      handleHide() {
-        this.visible = false
-      },
-      onUpdateImgGallery(src) {
-        this.image_gallery.push(src)
-      }
+    handleHide() {
+      this.visible = false
     },
-  }
+    onUpdateImgGallery(src) {
+      this.image_gallery.push(src)
+    }
+  },
+}
 </script>
-
-<style scoped>
-  .gallery {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
-    grid-gap: 1rem;
-    max-width: 80rem;
-    margin: 2rem auto;
-    padding: 0 5rem;
-  }
-
-  .gallery-panel img {
-    width: 100%;
-    object-fit: inside;
-    cursor: pointer;
-  }
-</style>
