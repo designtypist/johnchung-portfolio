bc293500932dd20976896cb401db5ae7b20a3222
James Chung <designtypist@gmail.com>
2020-10-21 13:47:35 -0400

    encrypted email from bot spammers, add social media links, changed the size of the animation video, update npm dependencies

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,8 @@
   <Layout>
 
     <div class="hero container mx-auto flex flex-col sm:flex-row justify-between">
-      <div class="text-2xl font-bold w-full text-center">
-        <h2 class="leading-tight py-4">Animation Reel</h2>
+      <div class="container-inner mx-auto w-full">
+        <h2 class="text-3xl font-bold text-center mb-6">Animation Reel</h2>
         <div class="iframe-container">
           <iframe src="https://player.vimeo.com/video/291040288?app_id=122963&amp;wmode=opaque" 
             title="JohnChung_Demoreel2018" id="yui_3_17_2_1_1581739318823_99" allowfullscreen></iframe>
